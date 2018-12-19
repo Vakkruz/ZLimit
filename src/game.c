06f5d81ac60a6f3805edb2455f53141b972447e6
Josh <jw299@njit.edu>
2018-12-18 22:04:28 -0500

    button presses work...sort of. very buggy and delayed

--- src/game.c
+++ src/game.c
@@ -12,13 +12,13 @@
 #include "gf3d_entity.h"
 #include "gf3d_gamepad.h"
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    int done = 0;
+	int done = 0;
 	double camera_bound = 0;
-    const Uint8 * keys;
-    Uint32 bufferFrame = 0;
-    VkCommandBuffer commandBuffer;
+	const Uint8 * keys;
+	Uint32 bufferFrame = 0;
+	VkCommandBuffer commandBuffer;
 
 	Entity *bot;
 	Entity *player;
@@ -30,49 +30,49 @@ int main(int argc,char *argv[])
 	Model *model3;
 
 	const int JOYSTICK_DEAD_ZONE = 8000;
-    
-    init_logger("gf3d.log");    
-    slog("gf3d begin");
-    gf3d_vgraphics_init(
-        "gf3d",                 //program name
-        1200,                   //screen width
-        700,                    //screen height
-        vector4d(0.51,0.75,1,1),//background color
-        0,                      //fullscreen
-        1                       //validation
-    );
+
+	init_logger("gf3d.log");
+	slog("gf3d begin");
+	gf3d_vgraphics_init(
+		"gf3d",                 //program name
+		1200,                   //screen width
+		700,                    //screen height
+		vector4d(0.51, 0.75, 1, 1),//background color
+		0,                      //fullscreen
+		1                       //validation
+	);
 	gamepad_start();
-    
-    // main game loop
-    slog("gf3d main loop begin");
+
+	// main game loop
+	slog("gf3d main loop begin");
 	//model = gf3d_model_load("agumon");
 	//model2 = gf3d_model_load("hedron");
-    model3 = gf3d_model_load("EGX");
+	model3 = gf3d_model_load("EGX");
 	//gf3d_vgraphics_rotate_camera_X(0);
 
-	
 
-    while(!done)
-    {
+
+	while (!done)
+	{
 		SDL_PollEvent(&e);
 
-        SDL_PumpEvents();   // update SDL's internal event structures
-        keys = SDL_GetKeyboardState(NULL); // get the keyboard state for this frame
-		
+		SDL_PumpEvents();   // update SDL's internal event structures
+		keys = SDL_GetKeyboardState(NULL); // get the keyboard state for this frame
 
-        //update game things here
-        
-        // configure render command for graphics command pool
-        // for each mesh, get a command and configure it from the pool
-        bufferFrame = gf3d_vgraphics_render_begin();
-        commandBuffer = gf3d_command_rendering_begin(bufferFrame);
+
+		//update game things here
+
+		// configure render command for graphics command pool
+		// for each mesh, get a command and configure it from the pool
+		bufferFrame = gf3d_vgraphics_render_begin();
+		commandBuffer = gf3d_command_rendering_begin(bufferFrame);
 
 		//gf3d_model_draw(model3,bufferFrame,commandBuffer);
 		//gf3d_model_draw(model2,bufferFrame,commandBuffer);
 		//draw_all_ents(bufferFrame, commandBuffer);
-			   		             
-        gf3d_command_rendering_end(commandBuffer);
-        gf3d_vgraphics_render_end(bufferFrame);
+
+		gf3d_command_rendering_end(commandBuffer);
+		gf3d_vgraphics_render_end(bufferFrame);
 
 
 		gamepad_checker(e);
@@ -91,27 +91,26 @@ int main(int argc,char *argv[])
 		//if (keys[SDL_SCANCODE_Z])gf3d_vgraphics_rotate_model(0.02);
 		//if (keys[SDL_SCANCODE_C])gf3d_vgraphics_rotate_model(-1 * 0.02);
 
-		
+
 		if (keys[SDL_SCANCODE_LCTRL] && (camera_bound > -15.0)) {
 			camera_bound -= 0.1;
 			//slog("%lf", camera_bound);
 			gf3d_vgraphics_zoom(camera_bound);
 		}
 
-		if(keys[SDL_SCANCODE_LALT] && (camera_bound < 15.0)) {
+		if (keys[SDL_SCANCODE_LALT] && (camera_bound < 15.0)) {
 			camera_bound += 0.1;
 			//slog("%lf", camera_bound);
 			gf3d_vgraphics_zoom(camera_bound);
 		}
 
-        if (keys[SDL_SCANCODE_ESCAPE])done = 1; // exit condition
-    }    
-    
-    vkDeviceWaitIdle(gf3d_vgraphics_get_default_logical_device());    
-    //cleanup
-    slog("gf3d program end");
-    slog_sync();
-    return 0;
-}
+		if (keys[SDL_SCANCODE_ESCAPE])done = 1; // exit condition
+	}
 
+	vkDeviceWaitIdle(gf3d_vgraphics_get_default_logical_device());
+	//cleanup
+	slog("gf3d program end");
+	slog_sync();
+	return 0;
+}
 /*eol@eof*/
--- src/gf3d_gamepad.c
+++ src/gf3d_gamepad.c
@@ -44,21 +44,77 @@ void gamepad_controls(SDL_Event eventer) {
 	if (SDL_PollEvent(&eventer) != 0) {
 		
 		if (eventer.type == SDL_CONTROLLERAXISMOTION) {
-			if (eventer.jaxis.which == 0) {
+			if (eventer.caxis.which == 0) {
+				//Left and right axies
 				if (eventer.caxis.axis == 0) {
 					//Left of dead zone
-					if (eventer.jaxis.value < -DEAD_ZONE)
+					if (eventer.caxis.value < -DEAD_ZONE)
 					{
 						slog("Moving left");
 					}
 					//Right of dead zone
-					else if (eventer.jaxis.value > DEAD_ZONE)
+					else if (eventer.caxis.value > DEAD_ZONE)
 					{
 						slog("Moving right");
+					}
+				//Up and down axies
+				}
+				else if (eventer.caxis.axis == 1) {
+					if (eventer.caxis.value < -DEAD_ZONE)
+					{
+						slog("Moving Up");
+					}
+					
+					else if (eventer.caxis.value > DEAD_ZONE)
+					{
+						slog("Moving Down");
 					}
 				}
 			}
 		}
+
+		
+		if (eventer.cbutton.state == SDL_PRESSED) {	
+			switch (eventer.cbutton.button) {
+
+				case SDL_CONTROLLER_BUTTON_A:
+					slog("A button pressed");
+					break;
+				case SDL_CONTROLLER_BUTTON_B:
+					slog("B button pressed");
+					break;
+				case SDL_CONTROLLER_BUTTON_X:
+					slog("X button pressed");
+					break;
+				case SDL_CONTROLLER_BUTTON_Y:
+					slog("Y button pressed");
+					break;
+				default:
+					slog("Button pressed");
+					break;
+			}
+		}else if (eventer.cbutton.state == SDL_RELEASED) {
+			switch (eventer.cbutton.button) {
+
+				case SDL_CONTROLLER_BUTTON_A:
+					slog("A button released");
+					break;
+				case SDL_CONTROLLER_BUTTON_B:
+					slog("B button released");
+					break;
+				case SDL_CONTROLLER_BUTTON_X:
+					slog("X button released");
+					break;
+				case SDL_CONTROLLER_BUTTON_Y:
+					slog("Y button released");
+					break;
+				default:
+					slog("Button released");
+					break;
+
+			}
+		}
+
 
 	}
 }
